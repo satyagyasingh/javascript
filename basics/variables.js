55a789cbd6d0f9b1d9719c117ba9cc18084e3168
satyagyasingh <satyagyasingh@gmail.com>
2024-05-17 01:28:46 +0530

    learned variables

---
+++ basics/variables.js
@@ -0,0 +1,30 @@
+const name = "satya"
+let age 
+
+console.log(typeof age)
+age = '7saal'
+
+let nulltype =  null
+// dont use ""var"" as they do not have funcctionality if scope
+let netWorth = 0
+console.log()
+console.table([name,typeof age])
+
+console.log(typeof nulltype)// object type while undefined is of type undefined
+
+
+// typecasting
+console.log(typeof netWorth,netWorth)
+
+netWorth = String(netWorth)
+
+console.log(typeof netWorth,netWorth)
+
+netWorth = "23rp"
+
+netWorth = Number(netWorth) 
+
+console.log(typeof netWorth , netWorth) //all characters inside netWorth string are not numbers so value will NaN  (not a number)
+
+netWorth = 23
+console.log(netWorth , "rupya")
